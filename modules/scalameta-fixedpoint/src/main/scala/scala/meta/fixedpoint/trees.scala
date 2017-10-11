abbb040d06fe502b39918deb9305bada8dad971b
andy scott <andyscott@users.noreply.github.com>
2017-10-11 10:07:48 -0700

    Switch to a zero-overhead hfix (#7)

--- modules/scalameta-fixedpoint/src/main/scala/scala/meta/fixedpoint/trees.scala
+++ modules/scalameta-fixedpoint/src/main/scala/scala/meta/fixedpoint/trees.scala
@@ -19,7 +19,7 @@ sealed trait TreeF[+A[_], +I]
 object TreeF extends TreeFInstances0 {
   /** This lets you call `.fix` on the leaf node trees with `A[_] = Nothing` */
   implicit final class FixTreeFOps[I](val tree: TreeF[HFix[TreeF, ?], I]) extends AnyVal {
-    def fix[II >: I]: HFix[TreeF, II] = HFix[TreeF, II](tree)
+    def fix[II >: I]: HFix[TreeF, II] = HFix.hfix[TreeF, II](tree)
   }
 }
 
